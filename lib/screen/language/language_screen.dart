5a8e07f11e485cc65f5d06206e1a314d32bcbdf3
skypeminh <minhhuyenbt@gmail.com>
2022-12-28 16:54:21 +0700

    Initial commit

--- lib/screen/language/language_screen.dart
+++ lib/screen/language/language_screen.dart
@@ -24,25 +24,93 @@ class _LanguageScreenState extends State<LanguageScreen> {
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
         child: Column(
           children: [
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-              decoration: BoxDecoration(
-                color: Primary_700.withOpacity(0.1),
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: Primary_700)
-              ),
-              width: 100,
-              child: Column(
-                children: [
-                  Image.asset(
-                    "assets/country/us.png",
-                    height: 80,
-                    width: 80,)
-                ],
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InkWell(
+                  onTap: (){},
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                    decoration: BoxDecoration(
+                        color: Primary_700.withOpacity(0.1),
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Primary_700)
+                    ),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          "assets/country/us.png",
+                          height: 64,
+                          width: 64,
+                        ),
+                        const SizedBox(
+                          height: 6,
+                        ),
+                        const Text("English",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+
+                InkWell(
+                  onTap: (){},
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                    decoration: BoxDecoration(
+                        color: Neutral_100,
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Neutral_300)
+                    ),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          "assets/country/india.png",
+                          height: 64,
+                          width: 64,
+                        ),
+                        const SizedBox(
+                          height: 6,
+                        ),
+                        const Text("Hindi",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+
+                InkWell(
+                  onTap: (){},
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                    decoration: BoxDecoration(
+                        color: Neutral_100,
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Neutral_300)
+                    ),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          "assets/country/russia.png",
+                          height: 64,
+                          width: 64,
+                        ),
+                        const SizedBox(
+                          height: 6,
+                        ),
+                        const Text("Russian",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             )
           ],
         ),
